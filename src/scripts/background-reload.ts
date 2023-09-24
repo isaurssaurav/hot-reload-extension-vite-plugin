a81206a60ee92ea4b9ba14dce4f1f624b9ad20b5
isaurssaurav <saurssaurav33@gmail.com>
2023-09-24 21:00:09 +0200

    fix: error log

--- src/scripts/background-reload.ts
+++ src/scripts/background-reload.ts
@@ -1,14 +1,18 @@
 import { HOT_RELOAD_EXTENSION_VITE_PORT, Message, isDev } from '../utils';
 
 if (isDev) {
-  const socket = new WebSocket(
-    `ws://localhost:${HOT_RELOAD_EXTENSION_VITE_PORT}`
-  );
+  try {
+    const socket = new WebSocket(
+      `ws://localhost:${HOT_RELOAD_EXTENSION_VITE_PORT}`
+    );
 
-  socket.addEventListener('message', (event) => {
-    if (event.data === Message.FILE_CHANGE) {
-      chrome.runtime.reload();
-      chrome.tabs.reload();
-    }
-  });
+    socket.addEventListener('message', (event) => {
+      if (event.data === Message.FILE_CHANGE) {
+        chrome.runtime.reload();
+        chrome.tabs.reload();
+      }
+    });
+  } catch (e) {
+    console.info('Please run `npm run dev` to start socket.', e);
+  }
 }
